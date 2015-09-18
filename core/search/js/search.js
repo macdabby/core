4f8066c5db94ef01bef7bb01675e54b3802ffe67
Jan-Christoph Borchardt <hey@jancborchardt.net>
2015-09-18 12:19:07 +0200

    fix wording of other search results to 'elsewhere'

--- core/search/js/search.js
+++ core/search/js/search.js
@@ -217,10 +217,10 @@
 					$status.addClass('emptycontent').removeClass('status');
 					$status.html('');
 					$status.append('<div class="icon-search"></div>');
-					$status.append('<h2>' + t('core', 'No search results in other places') + '</h2>');
+					$status.append('<h2>' + t('core', 'No search results elsewhere') + '</h2>');
 				} else {
 					$status.removeClass('emptycontent').addClass('status');
-					$status.text(n('core', '{count} search result in other places', '{count} search results in other places', count, {count:count}));
+					$status.text(n('core', '{count} search result elsewhere', '{count} search results elsewhere', count, {count:count}));
 				}
 			}
 			function renderCurrent() {
